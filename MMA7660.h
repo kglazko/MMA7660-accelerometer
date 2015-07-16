928958b9a4b46ef2003bc4e65793df2d1f2ec60e
wesner0019 <wesner0019@gmx.com>
2015-07-16 12:05:56 -0500

    updated

--- MMA7660.h
+++ MMA7660.h
@@ -1,46 +1,68 @@
-/*
- * MMA7660.h - Interface a MMA7660 accelerometer to an AVR via i2c
- * Version 0.1 - http://www.timzaman.com/
- * Copyright (c) 2011 Tim Zaman
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS
- * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
- * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
- * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- */
+/*****************************************************************************/
+// Function: Header file for class MMC7660
+// Hardware: Grove - 3-Axis Digital Accelerometer(±1.5g)
+// Arduino IDE: Arduino-1.0
+// Author: Frankie.Chu
+// Date: Jan 10,2013
+// Version: v0.9b
+// by www.seeedstudio.com
+//
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+//
+/*******************************************************************************/
+#ifndef __MMC7660_H__
+#define __MMC7660_H__
 
-//#include "Arduino.h"
+#include "application.h"
 
-#ifndef MMA7660_h
-#define MMA7660_h
+#define MMA7660_ADDR 0x4C
 
-class MMA
+#define MMA7660_X 0x00
+#define MMA7660_Y 0x01
+#define MMA7660_Z 0x02
+#define MMA7660_TILT 0x03
+#define MMA7660_SRST 0x04
+#define MMA7660_SPCNT 0x05
+#define MMA7660_INTSU 0x06
+#define MMA7660_MODE 0x07
+#define MMA7660_STAND_BY 0x00
+//#define MMA7660_ACTIVE 0x01
+#define MMA7660_ACTIVE 0x01
+#define MMA7660_SR 0x08 //sample rate register
+#define AUTO_SLEEP_120 0X00//120 sample per second
+#define AUTO_SLEEP_64 0X01
+#define AUTO_SLEEP_32 0X02
+#define AUTO_SLEEP_16 0X03
+#define AUTO_SLEEP_8 0X04
+#define AUTO_SLEEP_4 0X05
+#define AUTO_SLEEP_2 0X06
+#define AUTO_SLEEP_1 0X07
+#define MMA7660_PDET 0x09
+#define MMA7660_PD 0x0A
+class MMA7660
 {
-  public:
-    MMA();
-    void init();
-    void getValues(int *x,int *y,int *z);
-  private:
-	void MMA7660_I2C_SEND(unsigned char REG_ADDRESS, unsigned  char DATA);
+private:
+void write(uint8_t _register, uint8_t _data);
+uint8_t read(uint8_t _register);
+public:
+void init();
+void setMode(uint8_t mode);
+void setSampleRate(uint8_t rate);
+unsigned char getXYZ(int8_t *x,int8_t *y,int8_t *z);
+unsigned char getAcceleration(float *ax,float *ay,float *az);
 };
 
-extern MMA MMA7660;
-
-#endif // MMA7660_h
+#endif
