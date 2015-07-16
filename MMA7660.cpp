43545e0bdfc89dbe2bbf47e8e7b3c7d693a0cf08
wesner0019 <wesner0019@gmx.com>
2015-07-16 12:04:29 -0500

    updated

--- MMA7660.cpp
+++ MMA7660.cpp
@@ -1,79 +1,151 @@
-/*
- * MMA7660.cpp - Interface a MMA thermometer to an AVR via i2c
- 
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY THE AUTHORhttps://www.spark.io/build/536ef65a557d8f6dc6000445/tab/MMA7660.cpp#verify AND CONTRIBUTORS
- * ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
- * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR CONTRIBUTORS
- * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- */
-#include "Wire.h"
-//#include "spark_wiring.h"
+/*****************************************************************************/
+// Function: Cpp file for class MMC7660
+// Hardware: Grove - 3-Axis Digital Accelerometer(±1.5g)
+// Arduino IDE: Arduino-1.0
+// Author: Frankie.Chu
+// Date: Jan 10,2013
+// Version: v0.9b
+// by www.seeedstudio.com
+//
+// modify by loovee
+// 2013-9-25
+// add time out funtion in getXYZ
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+//
+/*******************************************************************************/
+//#include <Wire.h>
+//#include <Arduino.h>
 #include "MMA7660.h"
-//#include "Arduino.h"
+/*Function: Write a byte to the register of the MMA7660*/
 
-
-#define MMA7660_ADDRESS 0x4C //This is the I2C address for our chip.
-
-int MMA_XYar[64] = {0,3,5,8,11,14,16,19,22,25,28,31,34,38,41,45,49,53,58,63,70,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,-80,-70,-63,-58,-53,-49,-45,-41,-38,-34,-31,-28,-25,-22,-19,-16,-14,-11,-8,-5,-3}; 
-int MMA_Zar[64] = {90,87,84,82,79,76,74,70,68,65,62,59,56,52,49,45,41,37,32,27,20,10,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,-10,-20,-27,-32,-37,-41,-45,-49,-52,-56,-59,-52,-65,-68,-71,-74,-76,-79,-82,-85,-87};
- 
-/* PUBLIC METHODS */
-
-MMA::MMA()
+void MMA7660::write(uint8_t _register, uint8_t _data)
 {
+    Wire.begin();
+    Wire.beginTransmission(MMA7660_ADDR);
+    Wire.write(_register);
+    Wire.write(_data);
+    Wire.endTransmission();
 }
-
-void MMA::init()
+/*Function: Read a byte from the regitster of the MMA7660*/
+uint8_t MMA7660::read(uint8_t _register)
 {
-	Wire.begin();       //Initiate the Wire library and join the I2C bus as a master	
-	MMA7660_I2C_SEND(0x07,0x00); // Setting up MODE to Stand by to set SR
-    MMA7660_I2C_SEND(0x06,0x10);
-    MMA7660_I2C_SEND(0x08,0x00);  // Setting up SR register to 120 samples active and auto sleep mode
-    MMA7660_I2C_SEND(0x07,0x01); //Setting up MODE Active to START measures
+    uint8_t data_read;
+    Wire.begin();
+    Wire.beginTransmission(MMA7660_ADDR);
+    Wire.write(_register);
+    Wire.endTransmission();
+    Wire.beginTransmission(MMA7660_ADDR);
+    Wire.requestFrom(MMA7660_ADDR,1);
+    while(Wire.available())
+    {
+        data_read = Wire.read();
+    }
+    Wire.endTransmission();
+    return data_read;
 }
 
-// This can be called at 100ms intervals to get new data
-void MMA::getValues(int *x,int *y,int *z)
+void MMA7660::init()
 {
-  Wire.beginTransmission(MMA7660_ADDRESS);
-  Wire.write(0x00);  // register to read
-  Wire.endTransmission();
-  Wire.requestFrom(MMA7660_ADDRESS, 3); // read a byte
-
-   if(Wire.available()){
-     *x=Wire.read();
-     *y=Wire.read();
-     *z=Wire.read();
-   }
-   *x=MMA_XYar[*x];
-   *y=MMA_XYar[*y];
-   *z=MMA_Zar[*z];
+    setMode(MMA7660_STAND_BY);
+    setSampleRate(AUTO_SLEEP_4);
+    //write(MMA7660_INTSU,0x03);
+    setMode(MMA7660_ACTIVE);
+    
 }
 
+void MMA7660::setMode(uint8_t mode)
+{
+    write(MMA7660_MODE,mode);
+}
+void MMA7660::setSampleRate(uint8_t rate)
+{
+    write(MMA7660_SR,rate);
+}
+/*Function: Get the contents of the registers in the MMA7660*/
+/* so as to calculate the acceleration. */
+unsigned char MMA7660::getXYZ(int8_t *x,int8_t *y,int8_t *z)
+{
 
-void MMA::MMA7660_I2C_SEND(unsigned char REG_ADDRESS, unsigned  char DATA){  //SEND data to MMA7660
-  Wire.beginTransmission(MMA7660_ADDRESS);
-  Wire.write(REG_ADDRESS);
-  Wire.write(DATA);
-  Wire.endTransmission();
+    unsigned char val[3];
+    int count = 0;
+    val[0] = val[1] = val[2] = 64;
+    
+    long timer1 = millis();
+    long timer2 = 0;
+    
+    while(Wire.available() > 0)
+    {
+        timer2 = millis();
+        if((timer2-timer1)>500)
+        {
+            return 0;
+        }
+    }
+
+    Wire.read();
+    Wire.requestFrom(MMA7660_ADDR,3);
+
+    timer1 = millis();
+    while(Wire.available())
+    {
+    
+        if(count < 3)
+        {
+
+            timer1 = millis();
+            while ( val[count] > 63 ) // reload the damn thing it is bad
+            {
+                val[count] = Wire.read();
+
+                timer2 = millis();
+                if((timer2-timer1)>50)
+                {
+                    return 0;
+                }
+
+            }
+        }
+
+        count++;
+        
+        timer2 = millis();
+        
+        if((timer2-timer1)>500)
+        {
+            return 0;
+        }
+
+    }
+    
+    *x = ((int8_t)(val[0]<<2))/4;
+    *y = ((int8_t)(val[1]<<2))/4;
+    *z = ((int8_t)(val[2]<<2))/4;
+    
+    return 1;
 }
 
+unsigned char MMA7660::getAcceleration(float *ax,float *ay,float *az)
+{
+    int8_t x,y,z;
+    
+    if(!getXYZ(&x, &y, &z))return 0;
+    *ax = (x/21.00);
+    *ay = (y/21.00);
+    *az = (z/21.00); //added - to 21 to switch up z valued for accel.
 
-// Set the default object
-MMA MMA7660 = MMA();
+    
+    return 1;
+    
+}
